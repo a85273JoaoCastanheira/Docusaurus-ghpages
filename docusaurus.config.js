c4aadcd2d4c743292107ff1cdf4cbe967426ae86
João Castanheira <a85273@ualg.pt>
2023-12-18 11:50:25 +0000

    Adição da pagina quem somos e serviços à navbar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/tp3-ghpages/',
+  baseUrl: '/docusaurus-ghpages/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
