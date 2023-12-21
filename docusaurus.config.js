d9601db71d80fbe1e72dada973ba47497594d23f
João Castanheira <a85273@ualg.pt>
2023-12-21 14:19:19 +0000

    Mudanças

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,11 +72,11 @@ const config = {
         },
         items: [
          //{to: '/pages', label: 'Quem Somos', position: 'left'},
-          //{
-          //href: "/sobre-nos", // The page path
-          //label: "Sobre Nós", // The link label
-          //position: "left", // The link position
-          //},
+          {
+          href: "/sobre-nos", // The page path
+          label: "Sobre Nós", // The link label
+          position: "left", // The link position
+          },
           {
           href: "/quem-somos", // The page path
           label: "Quem Somos", // The link label
@@ -88,6 +88,7 @@ const config = {
             position: 'left',
             label: 'Serviços',
           },
+
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
